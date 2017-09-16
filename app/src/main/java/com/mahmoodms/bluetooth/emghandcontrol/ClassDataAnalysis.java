371d110c04816bb3a9443950f963dca15ac97c51
AD\mmahmood31 <musasmahmood@gmail.com>
2017-09-16 18:35:51 -0400

    Modified DCA to load most recent training file by default.

--- app/src/main/java/com/mahmoodms/bluetooth/emghandcontrol/ClassDataAnalysis.java
+++ app/src/main/java/com/mahmoodms/bluetooth/emghandcontrol/ClassDataAnalysis.java
@@ -1,6 +1,7 @@
 package com.mahmoodms.bluetooth.emghandcontrol;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.common.primitives.Doubles;
 
@@ -13,53 +14,34 @@ import java.util.List;
 public class ClassDataAnalysis {
     private static final String TAG = ClassDataAnalysis.class.getSimpleName();
     private static double data[][];
-
-//    public ClassDataAnalysis(List<String[]> strings) {
-//        int stringSize = strings.size();
-//        data = new double[4][stringSize];
-//        //Check data integrity first:
-//        int END = stringSize;
-//        for (int i = 0; i < stringSize; i++) {
-//            if(strings.get(i).length!=4) {
-//                END = i;
-//                Log.e(TAG, "INCORRECT LENGTH("+String.valueOf(strings.get(i).length)+")!; BREAK @ [ " + String.valueOf(END) + "/" + String.valueOf(stringSize) + "]");
-////                return;
-//                break;
-//            }
-//        }
-//
-//        for (int i = 0; i < END; i++) {
-//            for (int j = 0; j < 4; j++) {
-//                //add to array
-//                if (strings.get(i).length==4)
-//                    data[j][i] = Double.parseDouble(strings.get(i)[j]);
-//            }
-//        }
-//        Log.e(TAG,"Final Len: ["+String.valueOf(data[0].length)+"]");
-//    }
+    public boolean ERROR = false;
 
     public ClassDataAnalysis(List<String[]> strings, int length) {
         int stringSize = strings.size();
         data = new double[4][length];
         //Check data integrity first:
-        for (int i = 0; i < length; i++) {
-            if(strings.get(i).length!=4) {
-                Log.e(TAG, "ERROR! - INCORRECT LENGTH("+String.valueOf(strings.get(i).length)+")!; BREAK @ [ " + String.valueOf(i) + "/" + String.valueOf(stringSize) + "]");
-                return;
+        if(strings.size() >= length) {
+            for (int i = 0; i < length; i++) {
+                if(strings.get(i).length!=4) {
+                    Log.e(TAG, "ERROR! - INCORRECT LENGTH("+String.valueOf(strings.get(i).length)+")!; BREAK @ [ " + String.valueOf(i) + "/" + String.valueOf(stringSize) + "]");
+                    return;
+                }
             }
-        }
-
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < 4; j++) {
-                //add to array
-                if (strings.get(i).length==4)
-                    data[j][i] = Double.parseDouble(strings.get(i)[j]);
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < 4; j++) { //add to array
+                    if (strings.get(i).length==4)
+                        data[j][i] = Double.parseDouble(strings.get(i)[j]);
+                }
             }
+            Log.e(TAG,"Final Len: ["+String.valueOf(data[0].length)+"]");
+            this.ERROR = false;
+        } else {
+            Log.e(TAG, "ERROR! - Not enough data. ");
+            this.ERROR = true;
         }
-        Log.e(TAG,"Final Len: ["+String.valueOf(data[0].length)+"]");
     }
 
-    public static double[] concatAll() {
+    static double[] concatAll() {
         if(data[0]!=null) return Doubles.concat(data[0], data[1], data[2], data[3]);
         else return null;
     }
